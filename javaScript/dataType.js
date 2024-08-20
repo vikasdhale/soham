23d761edd2e9604aaff605b32c4b53caadc755d1
vikasdhale <vikasdhale89@gmail.com>
2024-08-20 15:18:46 +0000

    dataType

---
+++ javaScript/dataType.js
@@ -0,0 +1,43 @@
+console.log("hello")
+// primitive data 
+
+// 7 data type
+//String , Number , Boolean ,null, undefined , symbol BigInt
+
+const score = 100
+const scoreValue = 100
+
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail;
+const id = Symbol('1234')
+const anotherId = Symbol('1234')
+
+console.log(id === anotherId);// ans is false
+
+
+const bigNumber = 435636788882n
+
+
+
+const hersos = ["vikas","sai","tejas"]
+ let  myObj ={
+    name : "vikas",
+    age : 20,
+}
+
+ const myfunction = function(){
+    console.log("hello");
+
+ }
+ console.log(typeof bigNumber);
+ console.log(typeof myfunction) // type is function
+console.log(typeof hersos) //type is object
+
+//Refrence // non primitive
+
+//Array ,Objects , Functions
+
+//all non primitive data its return type is object
